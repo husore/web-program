abe7bbb91227a1ec15716ac0eae0ad1ecbcda980
Ruslan <leopardww@gmail.com>
2023-02-28 16:11:23 +0300

    feat: make page About

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var typingEffect = new Typed(".multiText",{
+    strings : ["coder", "student", "designer"],
+    loop : true,
+    typeSpeed : 100,
+    backSpeed : 80,
+    startDelay : 1000,
+    backDelay : 1500
+})
